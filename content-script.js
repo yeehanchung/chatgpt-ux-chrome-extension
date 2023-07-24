870b694e0c37b40ca2f8f21aa32987edc0aa3a35
yeehanchung <cyh0011215@gmail.com>
2023-07-24 22:28:43 +0800

    feat: support Slash (focus) and Escape (blur) keys for chatgpt prompt textarea

---
+++ content-script.js
@@ -0,0 +1,13 @@
+window.addEventListener("keydown", function (event) {
+    const nodePromptTextArea = document.getElementById("prompt-textarea");
+    const isFocusingOnPromptTextArea = nodePromptTextArea.contains(document.activeElement)
+  
+    if (!isFocusingOnPromptTextArea && event.key === "/") {
+      event.preventDefault();
+      nodePromptTextArea.focus();
+    }
+  
+    if (isFocusingOnPromptTextArea && event.key === "Escape") {
+      nodePromptTextArea.blur();
+    }
+  });
