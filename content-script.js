59b8e60f566fed6281f9c855a9ede0dba5449342
YeeHan <cyh0011215@gmail.com>
2023-09-02 14:11:00 +0800

    fix(content-script): return early if null

--- content-script.js
+++ content-script.js
@@ -1,13 +1,16 @@
 window.addEventListener("keydown", function (event) {
-    const nodePromptTextArea = document.getElementById("prompt-textarea");
-    const isFocusingOnPromptTextArea = nodePromptTextArea.contains(document.activeElement)
-  
-    if (!isFocusingOnPromptTextArea && event.key === "/") {
-      event.preventDefault();
-      nodePromptTextArea.focus();
-    }
-  
-    if (isFocusingOnPromptTextArea && event.key === "Escape") {
-      nodePromptTextArea.blur();
-    }
-  });
+  const nodePromptTextArea = document.getElementById("prompt-textarea");
+  if (!nodePromptTextArea) {
+    return;
+  }
+  const isFocusingOnPromptTextArea = nodePromptTextArea.contains(document.activeElement)
+
+  if (!isFocusingOnPromptTextArea && event.key === "/") {
+    event.preventDefault();
+    nodePromptTextArea.focus();
+  }
+
+  if (isFocusingOnPromptTextArea && event.key === "Escape") {
+    nodePromptTextArea.blur();
+  }
+});
